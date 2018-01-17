d091b6b16c835c45c4fabb7bce2f0d595a4ab7a4
iamjosephvarghese <josephvarghese.rms@gmail.com>
2018-01-17 23:19:55 +0530

    firebase fetching working

--- app/src/main/java/rm/iamjosephvarghese/deadlock2k18/MainActivity.java
+++ app/src/main/java/rm/iamjosephvarghese/deadlock2k18/MainActivity.java
@@ -2,6 +2,13 @@ package rm.iamjosephvarghese.deadlock2k18;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +16,25 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+        final FirebaseDatabase database = FirebaseDatabase.getInstance();
+        DatabaseReference ref = database.getReference("players");
+
+
+        ref.child("id1").addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+                Log.d("val",dataSnapshot.getValue().toString());
+            }
+
+            @Override
+            public void onCancelled(DatabaseError databaseError) {
+
+            }
+        });
+
+
+
     }
 }
