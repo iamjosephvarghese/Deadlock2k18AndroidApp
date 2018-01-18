a06abc4a5ba454d1259bb8540425973a5a3d8fb7
iamjosephvarghese <josephvarghese.rms@gmail.com>
2018-01-18 11:44:13 +0530

    interface added for blocking execution....other code commented....not tested....may have erors

--- app/src/main/java/rm/iamjosephvarghese/deadlock2k18/MainActivity.java
+++ app/src/main/java/rm/iamjosephvarghese/deadlock2k18/MainActivity.java
@@ -3,7 +3,11 @@ package rm.iamjosephvarghese.deadlock2k18;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.widget.ImageView;
 
+import com.bumptech.glide.Glide;
+import com.firebase.client.Firebase;
+import com.firebase.client.FirebaseError;
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
@@ -15,55 +19,161 @@ public class MainActivity extends AppCompatActivity {
     int level;
     String url;
 
+    ImageView questionImage;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
 
+        Firebase.setAndroidContext(this);
+
         final FirebaseDatabase database = FirebaseDatabase.getInstance();
         DatabaseReference ref = database.getReference("players");
-        DatabaseReference questionRef = database.getReference("images");
-        DatabaseReference answerRef = database.getReference("answers");
+       // DatabaseReference imageRef = database.getReference("images");
+       // DatabaseReference answerRef = database.getReference("answers");
 
 
-        ref.child("id1").addValueEventListener(new ValueEventListener() {
+        questionImage = findViewById(R.id.questionImage);
+
+
+        readData(ref.child("id1"), new OnGetDataListener() {
             @Override
-            public void onDataChange(DataSnapshot dataSnapshot) {
+            public void onSuccess(DataSnapshot dataSnapshot) {
+
                 level = dataSnapshot.getValue(Integer.class);
-                Log.d("level",dataSnapshot.getValue().toString());
+                Log.d("level",Integer.toString(level));
+                gotLevel();
             }
 
             @Override
-            public void onCancelled(DatabaseError databaseError) {
+            public void onStart() {
 
+                Log.d("OnStart","started");
+            }
+
+            @Override
+            public void onFailure() {
+
+                Log.d("Onfaliure","faliure");
             }
         });
 
 
+//        ref.child("id1").addValueEventListener(new ValueEventListener() {
+//            @Override
+//            public void onDataChange(DataSnapshot dataSnapshot) {
+//                level = dataSnapshot.getValue(Integer.class);
+//                Log.d("level",dataSnapshot.getValue().toString());
+//            }
+//
+//            @Override
+//            public void onCancelled(DatabaseError databaseError) {
+//
+//            }
+//        });
 
 
-        questionRef.child("1").addValueEventListener(new ValueEventListener() {
+
+
+        /////
+
+
+//        Glide.with(this).load(url).into(questionImage);
+
+
+
+
+    }
+
+
+
+    public interface OnGetDataListener {
+        //make new interface for call back
+        void onSuccess(DataSnapshot dataSnapshot);
+        void onStart();
+        void onFailure();
+    }
+
+    public void readData(DatabaseReference ref, final OnGetDataListener listener) {
+        listener.onStart();
+//        ref.addListenerForSingleValueEvent(new ValueEventListener() {
+//            @Override
+//            public void onDataChange(DataSnapshot dataSnapshot) {
+//                listener.onSuccess(dataSnapshot);
+//            }
+//
+//            @Override
+//            public void onCancelled(FirebaseError firebaseError) {
+//                listener.onFailure();
+//            }
+//        });
+
+        ref.addListenerForSingleValueEvent(new ValueEventListener() {
             @Override
             public void onDataChange(DataSnapshot dataSnapshot) {
+                listener.onSuccess(dataSnapshot);
+            }
+
+            @Override
+            public void onCancelled(DatabaseError databaseError) {
+                listener.onFailure();
+            }
+        });
+
+    }
+
+
+    public void gotLevel(){
+
+        final FirebaseDatabase database = FirebaseDatabase.getInstance();
+
+
+        DatabaseReference imageRef = database.getReference("images");
+
+//        imageRef.child("1").addValueEventListener(new ValueEventListener() {
+//            @Override
+//            public void onDataChange(DataSnapshot dataSnapshot) {
+//                url = dataSnapshot.getValue(String.class);
+//                Log.d("url",url);
+//
+//            }
+//
+//            @Override
+//            public void onCancelled(DatabaseError databaseError) {
+//
+//            }
+//        });
+
+
+        readData(imageRef, new OnGetDataListener() {
+            @Override
+            public void onSuccess(DataSnapshot dataSnapshot) {
                 url = dataSnapshot.getValue(String.class);
                 Log.d("url",url);
+                gotUrl(url);
+            }
+
+            @Override
+            public void onStart() {
 
             }
 
             @Override
-            public void onCancelled(DatabaseError databaseError) {
+            public void onFailure() {
 
             }
         });
 
 
 
+    }
 
 
 
-
-
+    public void gotUrl(String url){
+        Glide.with(this).load(url).into(questionImage);
 
     }
 }
