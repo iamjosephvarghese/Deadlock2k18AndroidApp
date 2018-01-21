491d4c7b489c2366589b46dda575f3d906f8ae18
iamjosephvarghese <josephvarghese.rms@gmail.com>
2018-01-21 17:24:15 +0530

    moving to Firestore....made Firestore the launcher activity....going to hashing method

--- app/src/main/java/rm/iamjosephvarghese/deadlock2k18/Firestore.java
+++ app/src/main/java/rm/iamjosephvarghese/deadlock2k18/Firestore.java
@@ -1,7 +1,18 @@
 package rm.iamjosephvarghese.deadlock2k18;
 
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.firestore.FirebaseFirestore;
+
+import java.security.MessageDigest;
+import java.util.Date;
+import java.util.HashMap;
+import java.util.Map;
 
 public class Firestore extends AppCompatActivity {
 
@@ -9,6 +20,31 @@ public class Firestore extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_firestore);
+
+
+        FirebaseFirestore db = FirebaseFirestore.getInstance();
+
+
+
+
+
+        Map<String,Object> playerDetails = new HashMap<>();
+        playerDetails.put("created",new Date());
+
+        db.collection("users").document("1").set(playerDetails).addOnSuccessListener(new OnSuccessListener<Void>() {
+            @Override
+            public void onSuccess(Void aVoid) {
+                Log.d("Success","..........");
+
+            }
+        }).addOnFailureListener(new OnFailureListener() {
+            @Override
+            public void onFailure(@NonNull Exception e) {
+                Log.d("Error","............");
+            }
+        });
+
+
 
 
 
